5224ee25eb87abc34507031262f2732c38a37126
Christopher White <mrchristopher124@gmail.com>
2012-12-14 10:27:10 -0500

    logging out should not be associated with sharing. if i need to log out, i will log out myself. this breaks the contract of what sharing says it will do which is to share. the method is not share and log out. this breaks facebook connect

--- Classes/ShareKit/Sharers/Services/Facebook/SHKFacebook.m
+++ Classes/ShareKit/Sharers/Services/Facebook/SHKFacebook.m
@@ -375,7 +375,6 @@ static SHKFacebook *requestingPermisSHKFacebook=nil;
         SHKSharer *iosSharer = [SHKiOSFacebook shareItem:self.item];
         iosSharer.quiet = self.quiet;
         iosSharer.shareDelegate = self.shareDelegate;
-        [SHKFacebook logout];
         
     } else {
         
